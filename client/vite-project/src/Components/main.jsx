b00ee773a1e8b92288bae80242f7ed08d8dbfb34
Benedict <palomakut1@gmail.com>
2025-03-26 22:37:38 +0000

    nice

--- client/vite-project/src/Components/main.jsx
+++ client/vite-project/src/Components/main.jsx
@@ -1,6 +1,6 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "../Styles/Styles.css";
+
 import "../Styles/main.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
